1e9a1faac7dc60188a4a473955080bc128cc267c
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-11-04 19:04:05 -0800

    button test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-// todo
+const button = document.getElementById("alertButton");
+
+if (button) {
+  button.addEventListener("click", () => {
+    alert("You clicked the button!");
+  });
+} else {
+  console.error("Button not found!");
+}
