6c8d4561ccc09bed11b0e4b5072700ac50a89e9d
amphy0 <106935682+amphy0@users.noreply.github.com>
2024-11-13 01:59:55 -0800

    d3.a done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,129 @@
-const button = document.getElementById("alertButton");
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
 
-if (button) {
-  button.addEventListener("click", () => {
-    alert("You clicked the button!");
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+import luck from "./luck.ts";
+
+// Location of our classroom (as identified on Google Maps)
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+let inventory = 0;
+
+//UI
+const app = document.getElementById("app")!;
+const gameName = "Geocoin Carrier";
+document.title = gameName;
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+const controlPanel = document.createElement("div");
+app.appendChild(controlPanel);
+
+const mapPanel = document.createElement("div");
+app.appendChild(mapPanel);
+mapPanel.id = "map";
+
+const statusPanel = document.createElement("div");
+app.appendChild(statusPanel);
+statusPanel.innerHTML = `Player has ${inventory} coins.`;
+
+const map = leaflet.map(mapPanel, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add a marker to represent the player
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers into lat/lng bounds
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [
+      origin.lat + (i + 1) * TILE_DEGREES,
+      origin.lng + (j + 1) * TILE_DEGREES,
+    ],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  // Handle interactions with the cache
+  rect.bindPopup(() => {
+    // Each cache has a random point value, mutable by the player
+    let pointValue = Math.floor(
+      luck([i, j, "initialValue"].toString()) * 100,
+    );
+
+    // The popup offers a description and button
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
+                <button id="collect">Collect</button><button id="deposit">Deposit</button>`;
+
+    popupDiv
+      .querySelector<HTMLButtonElement>("#collect")!
+      .addEventListener("click", () => {
+        if (pointValue > 0) {
+          pointValue--;
+          popupDiv.querySelector<HTMLSpanElement>(
+            "#value",
+          )!.innerHTML = pointValue.toString();
+          inventory++;
+          statusPanel.innerHTML = `Player has ${inventory} coins.`;
+        }
+      });
+    popupDiv
+      .querySelector<HTMLButtonElement>("#deposit")!
+      .addEventListener("click", () => {
+        if (inventory > 0) {
+          pointValue++;
+          popupDiv.querySelector<HTMLSpanElement>(
+            "#value",
+          )!.innerHTML = pointValue.toString();
+          inventory--;
+          statusPanel.innerHTML = `Player has ${inventory} coins.`;
+        }
+      });
+
+    return popupDiv;
   });
-} else {
-  console.error("Button not found!");
+}
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
 }
